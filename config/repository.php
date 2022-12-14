cad02e11ebc2d6a7fc7a53528b6724fc3e2a03b8
hakim-asrori <saneglos005@gmail.com>
2022-12-14 20:43:34 +0700

    add command create repository

--- config/repository.php
+++ config/repository.php
@@ -10,4 +10,14 @@ return [
    * Default repository namespace
    */
   "repository_namespace" => "App\Http\Repositories",
+
+  /**
+   * The directory for all the repositories
+   */
+  "contract_directory" => "app/Http/Repositories/Contracts",
+
+  /**
+   * Default contract namespace
+   */
+  "contract_namespace" => "App\Http\Repositories\Contracts",
 ];
--- src/Commands/MakeRepositoryCommand.php
+++ src/Commands/MakeRepositoryCommand.php
@@ -2,23 +2,30 @@
 
 namespace Hakimasrori\Repository\Commands;
 
+use Hakimasrori\Repository\Traits\CreateFile;
+use Hakimasrori\Repository\Traits\NamespaceFixer;
 use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\File;
 
 class MakeRepositoryCommand extends Command
 {
+  use NamespaceFixer;
+
   /**
    * The name and signature of the console command.
    *
    * @var string
    */
-  protected $signature = 'make:repository';
+  protected $signature = 'make:repository {name : The name of the repository}';
 
   /**
    * The console command description.
    *
    * @var string
    */
-  protected $description = 'Command description';
+  protected $description = 'Create new repository class';
 
   /**
    * Execute the console command.
@@ -27,6 +34,93 @@ class MakeRepositoryCommand extends Command
    */
   public function handle()
   {
-    return Command::SUCCESS;
+    $repositoryName = $this->argument('name');
+    if ($repositoryName === '' || is_null($repositoryName) || empty($repositoryName)) {
+      $this->error('Name Invalid..!');
+    }
+
+    $isContractCreated = $this->createContract($repositoryName);
+    if (!$isContractCreated) {
+      return;
+    }
+
+    $this->createRepository($repositoryName);
+  }
+
+  protected function createContract($name)
+  {
+    if (!File::exists($this->getBaseDirectory("Contracts\\" . $name))) {
+      File::makeDirectory($this->getBaseDirectory("Contracts\\" . $name), 0777, true);
+    }
+
+    $title = $this->title($name) . config('repository-name.repository_interface_suffix');
+    $baseName = $this->getBaseFileName($name) . config('repository-name.repository_interface_suffix');
+
+    $contractPath = config('repository.contract_directory') . '/' . $title;
+    $filePath = $contractPath . '.php';
+    $contractNamespacePath = $filePath;
+
+    $stubProperties = [
+      "{contractName}" => $baseName
+    ];
+
+    if (!File::exists($filePath)) {
+      $fileContent = __DIR__ . "/stub/repository-contract.stub";
+
+      new CreateFile($stubProperties, $contractNamespacePath, $fileContent);
+
+      $this->info('Contract Created Successfully.');
+      return true;
+    }
+  }
+
+  protected function createRepository($name)
+  {
+    if (!File::exists($this->getBaseDirectory($name))) {
+      File::makeDirectory($this->getBaseDirectory($name), 0775, true);
+    }
+
+    $titleContract = $this->title($name) . config('repository-name.repository_interface_suffix');
+    $contract = $this->getBaseFileName($name) . config('repository-name.repository_interface_suffix');
+
+    $contractPath = config('repository.contract_directory') . '/' . $titleContract;
+    $contractNamespace = Str::ucfirst($this->getNameSpace($contractPath));
+
+    $title = $this->getBaseFileName($name);
+    $titleRepository = $this->title($name) . config("repository-name.repository_suffix");
+    $baseName = $this->getBaseFileName($name) . config("repository-name.repository_suffix");
+
+    $repoPath = config('repository.repository_directory') . $titleRepository;
+    $filePath = $repoPath . ".php";
+
+    $repositoryNamespacePath = $filePath;
+
+    $stubProperties = [
+      "{contractName}" => $contract,
+      "{repositoryName}" => $baseName,
+      "{model}" => $title,
+      "{modelVariable}" => Str::camel($title),
+    ];
+
+    if (!File::exists($filePath)) {
+      $fileContent = __DIR__ . "/stub/repository.stub";
+
+      new CreateFile(
+        $stubProperties,
+        $repositoryNamespacePath,
+        $fileContent
+      );
+
+      $this->info('Repository Created Successfully.');
+      return true;
+    } else {
+      $this->error('Repository Already Exists.');
+      return false;
+    }
+  }
+
+  protected function title($name)
+  {
+    return Str::remove(' ', ucwords(Str::of($name)->replace('_', ' ')));
   }
 }
