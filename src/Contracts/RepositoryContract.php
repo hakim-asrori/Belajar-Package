0289af0bbf7c0fccfa1652483521a3b270a81e44
hakim-asrori <saneglos005@gmail.com>
2022-12-16 11:10:32 +0700

    update repository

--- src/Contracts/RepositoryContract.php
+++ src/Contracts/RepositoryContract.php
@@ -7,17 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 interface RepositoryContract
 {
-  public function all(): Collection;
+    public function all(): Collection;
 
-  public function findWhere(array $attributes): Model;
+    public function findWhere(array $attributes): ?Model;
 
-  public function getWhere(array $attributes): Collection;
+    public function getWhere(array $attributes): Collection;
 
-  public function create(array $attributes): Model;
+    public function create(array $attributes): Model;
 
-  public function update(array $attributes, int $id): Model;
+    public function update(array $attributes, int $id);
 
-  public function delete(int $id): bool;
+    public function delete(int $id): bool;
 
-  public function find(int $id): ?Model;
+    public function find(int $id): ?Model;
 }
--- src/Repository.php
+++ src/Repository.php
@@ -8,48 +8,48 @@ use Illuminate\Database\Eloquent\Model;
 
 class Repository implements RepositoryContract
 {
-  protected $model;
+    protected $model;
 
-  public function __construct(Model $model)
-  {
-    $this->model = $model;
-  }
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
 
-  public function all(): Collection
-  {
-    return $this->model->all();
-  }
+    public function all(): Collection
+    {
+        return $this->model->all();
+    }
 
-  public function findWhere(array $attributes): Model
-  {
-    return $this->model->where($attributes)->first();
-  }
+    public function findWhere(array $attributes): ?Model
+    {
+        return $this->model->where($attributes)->first();
+    }
 
-  public function getWhere(array $attributes): Collection
-  {
-    return $this->model->where($attributes)->get();
-  }
+    public function getWhere(array $attributes): Collection
+    {
+        return $this->model->where($attributes)->get();
+    }
 
-  public function create(array $attributes): Model
-  {
-    return $this->model->create($attributes);
-  }
+    public function create(array $attributes): Model
+    {
+        return $this->model->create($attributes);
+    }
 
-  public function update(array $attributes, int $id): Model
-  {
-    $model = $this->find($id);
-    $model->update($attributes);
-    return $model;
-  }
+    public function update(array $attributes, int $id)
+    {
+        $model = $this->find($id);
+        $model->update($attributes);
+        return $model;
+    }
 
-  public function delete(int $id): bool
-  {
-    $model = $this->find($id);
-    return $model->delete();
-  }
+    public function delete(int $id): bool
+    {
+        $model = $this->find($id);
+        return $model->delete();
+    }
 
-  public function find(int $id): ?Model
-  {
-    return $this->model->findOrFail($id);
-  }
+    public function find(int $id): ?Model
+    {
+        return $this->model->findOrFail($id);
+    }
 }
